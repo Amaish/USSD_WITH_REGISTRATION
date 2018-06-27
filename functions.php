6ea3dc7b3a69a7999acd8cd961840fd6af74f199
Anthony <anthonymaina.nganga@gmail.com>
2018-06-27 16:59:05 +0300

    changes

--- functions.php
+++ functions.php
@@ -28,9 +28,45 @@ function getByValue($table, $column, $arguments)
     {
         $getValues = mysqli_fetch_array($executeQuery);
         return $getValues[$column];
-    } else {
+    } 
+    else {
         return false;
     }
+}
+
+function UnregisteredWelcomeScreen()
+{
+    $response  = "CON Welcome to test USSD\n";
+    $response .= "Do you wish to register?\n";
+    $response .= "1. Yes\n";
+    $response .= "2. No";
+    return $response;
+}
+function RegisterUser()
+{
+    $response = "CON Please enter your name";
+    return $response;
+}
+
+function RegisterdUserWelcomeScreen($name)
+{
+    $response  = "CON Welcome $name what would you like to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number \n";
+    $response .= "3. Edit name \n";
+    return $response;
+}
+function NameChangeSuccess()
+{
+    $response  = "CON Proceed?\n";
+    $response .= "1. Yes \n";
+    $response .= "2. No";
+    return $response;
+}
+function ExitRegisteredUser($name)
+{
+    $response = "END Good-Bye $name";
+    return $response;
 }
 
 ?>
